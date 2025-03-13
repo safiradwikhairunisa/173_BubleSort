fb6756a42dbff6667c42026f73d10ce36891006d
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-03-13 11:05:36 +0700

    menambahkan cout

--- bubleShort.cpp
+++ bubleShort.cpp
@@ -42,6 +42,21 @@ void bubleSortArray(){
     }while(pass <= n-1);  //step 5
 }
 
+
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1){
+            cout << "--> ";
+        }
+    }
+
+}
 int main () {
 
 }
