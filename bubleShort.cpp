ab08dff469c081caa37cdb8049dbacc6a0f607ea
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-03-13 11:29:40 +0700

    menghapus void display

--- bubleShort.cpp
+++ bubleShort.cpp
@@ -41,19 +41,7 @@ void bubleSortArray(){
         pass = pass + 1;  //step 4
     }while(pass <= n-1);  //step 5
 }
-void display(){
-    cout << endl;
-    cout << "=================================" << endl;
-    cout << "Element Array yang telah tersusun" << endl;
-    cout << "=================================" << endl;
-    cout << endl;
-    for (int j=0;j<n;j++){
-        cout << arr[j];
-        if (j<n-1){
-            cout << "--> ";
-        }
-    }
-}
+
 
 int main () {
     input();
